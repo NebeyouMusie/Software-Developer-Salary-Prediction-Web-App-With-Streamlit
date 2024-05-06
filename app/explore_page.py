689e3ee653b65e9443568bc4bf39aacc22a38492
Nebeyou Musie <Nebeyoumusie@gmail.com>
2024-05-06 09:44:18 +0000

    added cleaned data

--- app/explore_page.py
+++ app/explore_page.py
@@ -34,22 +34,7 @@ def clean_education(x):
 
 st.cache_data
 def load_data():
-    df = pd.read_csv('../survey_results_public.csv')
-    df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedCompYearly"]]
-    df = df[df["ConvertedCompYearly"].notnull()]
-    df = df.dropna()
-    df = df[df["Employment"] == "Employed full-time"]
-    df = df.drop("Employment", axis=1)
-
-    country_map = shorten_categories(df.Country.value_counts(), 400)
-    df["Country"] = df["Country"].map(country_map)
-    df = df[df["ConvertedCompYearly"] <= 250000]
-    df = df[df["ConvertedCompYearly"] >= 10000]
-    df = df[df["Country"] != "Other"]
-
-    df["YearsCodePro"] = df["YearsCodePro"].apply(clean_experience)
-    df["EdLevel"] = df["EdLevel"].apply(clean_education)
-    df = df.rename({"ConvertedCompYearly": "Salary"}, axis=1)
+    df = pd.read_csv('../cleaned_data.csv')
     return df
 
 df = load_data()
